10c4d1545fca1e4bd642368096ca339f27fd0616
jacksonjr11 <jackson.augusto.1571@gmail.com>
2021-12-29 09:11:39 -0300

    update project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
+import style from "../styles/home.module.scss";
+
 export default function Home() {
-  return (
-     <h1>Helloo World</h1>
-  )
+  return <h1 className={style.title}>Helloo World</h1>;
 }
