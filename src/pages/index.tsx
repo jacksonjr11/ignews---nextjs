caf4685b459ed2bffef51f45da11938abeae62b7
jacksonjr11 <jackson.augusto.1571@gmail.com>
2022-01-02 12:27:49 -0300

    update project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,6 +47,6 @@ export const getStaticProps: GetStaticProps = async () => {
   };
   return {
     props: { product },
-    revalidate: 60 * 60 * 24, // 24 hours
+    revalidate: 60 * 60 * 24,  // 24 hours
   };
 };
