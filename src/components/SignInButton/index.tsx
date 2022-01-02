39ce51ab9c19dd49126349320b362fd7d32b2c40
jacksonjr11 <jackson.junior@livsaude.com.br>
2022-01-02 11:20:19 -0300

    created component SubscribeButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,9 +1,19 @@
+import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 
 export function SignInButton() {
-  return (
-    <button type="button">
-      <FaGithub />
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Jackson Junior
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
       Sign in with Github
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig-news</title>
+        <title>Home | ig-news</title>
       </Head>
-      <h1>Helloo World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications
+            <span> for $9.90 months.</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
