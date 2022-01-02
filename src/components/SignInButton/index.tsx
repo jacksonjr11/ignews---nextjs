445cd71c5a99f792d7e5a8d2e247dbe4faa13340
jacksonjr11 <jackson.junior@livsaude.com.br>
2022-01-02 09:24:04 -0300

    update project

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,10 @@
+import { FaGithub } from "react-icons/fa";
+
+export function SignInButton() {
+  return (
+    <button type="button">
+      <FaGithub />
+      Sign in with Github
+    </button>
+  );
+}
